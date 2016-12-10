bc4529dc90412d003197fe1f49e498ab0e3ecc66
Andrew Yip <yipcma@gmail.com>
2016-12-11 01:41:19 +0300

    use start_urls shortcut for request

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,14 +2,10 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
+    start_urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
         ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
